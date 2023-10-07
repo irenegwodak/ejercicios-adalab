1cc86231305861d36b3a9339bd9d02ebca35415a
Irene García <irenegwodak@gmail.com>
2023-10-07 18:54:45 +0200

    Add hangman-game

---
+++ module3-hangman-game/src/components/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import reactLogo from '../assets/react.svg';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
